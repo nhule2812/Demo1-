13a43af7c0aa79d3bb1789a9d420aba82dee182b
nhule2812 <lebuithanhnhu28122005@gmail.com>
2024-10-22 00:51:39 +0700

    init project

---
+++ Areas/admin/Controllers/ProductsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using laptrinhweb.Models;
+using laptrinhweb.Models.ViewModel;
+
+namespace laptrinhweb.Areas.admin.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private MyStoreEntities db = new MyStoreEntities();
+        
+        // GET: admin/Products
+        //public ActionResult Index(string searchTerm)
+        //{
+        //    var products = db.Products.Include(p => p.Category);
+        //    return View(products.ToList());
+        //}
+        public ActionResult Index(string searchTerm)
+        {
+            var model = new ProductSearchVM();
+            var products = db.Products.AsQueryable();
+            if (!string.IsNullOrEmpty(searchTerm))
+            { // tìm kiếm sản phẩm dựa tren từ khóa
+                products = products.Where(p =>
+                                    p.ProductName.Contains(searchTerm) ||
+                                    p.ProductDecription.Contains(searchTerm) ||
+                                    p.Category.CategoryName.Contains(searchTerm));
+            }    
+            // tìm kiếm sản phẩm dựa trên giá tối thiểu 
+            if (minPrice.HasValue)
+            {
+                products = products.Where(p =>p.ProductPrice <= minPrice.Value);
+            }    
+            // tìm kiếm sản phẩm dựa trên giá tối đa
+            if(maxPrice.HasValue)
+            {
+                products = products.Where(p => p.ProductPrice <= maxPrice.Value);
+            }    
+            model.Products= products.ToList();
+            return View(model);
+        }
+
+        // GET: admin/Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: admin/Products/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName");
+            return View();
+        }
+
+        // POST: admin/Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductID,CategoryID,ProductName,ProductDecription,ProductPrice,ProductImage")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
+            return View(product);
+        }
+
+        // GET: admin/Products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
+            return View(product);
+        }
+
+        // POST: admin/Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductID,CategoryID,ProductName,ProductDecription,ProductPrice,ProductImage")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
+            return View(product);
+        }
+
+        // GET: admin/Products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: admin/Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/ViewModel/ProductSearchVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace laptrinhweb.Models.ViewModel
+{
+    public class ProductSearchVM
+    {
+        //tiêu chí để search theo tên, mô tả sp 
+        // hoặc loại sản phẩm 
+        public string SearchTerm { get; set; }
+        public List<Product> Products { get; set; }
+        // các tiêu chí  để search theo giá 
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+        // thứ tự sắp xếp 
+        public string SortOder {  get; set; }
+         // các thuộc tính hỗ trợ phân trang 
+         public int PageNumber { get; set; }   // trang hiện tại 
+        public int PageSize { get; set; } = 10; // số sản phẩm mỗi trang 
+        // danh sách sản phẩm đã phân trang 
+       
+    }
+}
